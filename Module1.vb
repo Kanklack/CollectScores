b156e1b78cd36a7ae3b5ba3b46d02c2d122f599b
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 20:11:49 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim scores(7) As Integer
+
+        'Dim game As Integer 
+
+        Console.WriteLine("Please enter 8 scores")
+
+        For i As Integer = 0 To 7
+
+            Console.WriteLine("Please enter score: ")
+
+            scores(i) = Convert.ToUInt16(Console.ReadLine())
+
+        Next
+
+        Console.WriteLine("All the scores you had entered were: ")
+
+        Console.Write("[")
+
+        For i As Integer = 0 To 7
+
+            Console.Write(scores(i) & ",")
+
+        Next
+
+        Console.Write("]")
+
+        Console.Read()
+
+    End Sub
+
+End Module
